b3fd4960c2b77669c2c86c169186ba57430c5bca
bootyocean18 <me@remysheppard.com>
2022-01-29 13:21:43 -0500

    Home Done?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   content: ["./content/**/*.md", "./content/**/*.html", "./layouts/**/*.html"],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
